6d2d96322439744292d592705990bdc0013fe720
IgorMalinovsky <i.malinovskij@gmail.com>
2021-01-01 15:39:28 +0200

    stop commit

--- stopwatch/src/app/app.component.ts
+++ stopwatch/src/app/app.component.ts
@@ -12,31 +12,48 @@ export class AppComponent {
   title = 'stopwatch';
   public displayTimer: any;
   public isRunning: boolean = false;
-  public startText = 'Start';
+  public startText = 'run/wait';
+  public startText2= 'start';
   public time: any;
-  
-
+  startStop(){
+    this.time = 0;
+    this.startText2 = 'start';
+    if (this.isRunning){
+      this.startText2 = 'start';
+      this.isRunning=false;
+    }else{
+      this.startText2 = 'stop';
+      this.isRunning=true;
+    }  
+};
+   resStop(){
+    this.ngOnInit();
+    this.getDisplayTimer(this.time);
+  }
   ngOnInit(): void {
     this.time = 0;
+    this.getDisplayTimer(this.time);
   }
-
  toggleTimer() {
     this.isRunning = !this.isRunning;
     this.stopwatch();
+    this.getDisplayTimer(this.time);
   }
-
  stopwatch() {
     timer(0, 1000).subscribe(ellapsedCycles => {
       if (this.isRunning) {
         this.time++;
         this.getDisplayTimer(this.time);
-        this.startText = 'Pause';
+        this.startText = 'wait/run';
+        this.startText2 = 'stop';
+        this.getDisplayTimer(this.time);
       } else {
-        this.startText = 'Start';
+        this.startText = 'run/wait';
+        this.startText2 = 'start';
+        this.getDisplayTimer(this.time);
       }
     });
   }
-
 getDisplayTimer(time: number) {
     var hours = '' + Math.floor(time / 3600);
     var minutes = '' + Math.floor(time % 3600 / 60);
@@ -57,14 +74,6 @@ getDisplayTimer(time: number) {
     } else {
       seconds = '' + seconds;
     }
-
     this.displayTimer = hours + ':' + minutes + ':' + seconds;
-  }
-
-
-
-
-
-
-  
+  } 
 }
